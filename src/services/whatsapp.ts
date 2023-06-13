74df924d4225d9df546154015337b32685ea162a
Renan Torres <rgrtorres@hotmail.com>
2023-06-13 19:23:39 -0300

    Mensagem de commit

--- src/services/whatsapp.ts
+++ src/services/whatsapp.ts
@@ -3,6 +3,8 @@ import * as qrcode from 'qrcode-terminal';
 import { resolve } from 'path';
 import dotenv from 'dotenv';
 
+var cron = require('node-cron');
+
 const client = new Client({
   authStrategy: new LocalAuth({
     clientId: 'wpp-bot',
@@ -15,9 +17,25 @@ const client = new Client({
 });
 
 client.on('qr', async (qr) => qrcode.generate(qr, { small: true }));
-client.on('authenticated', () => console.log('WhatsApp authenticated.'));
+client.on('authenticated', () => {
+  cron.schedule('0 8 * * 1-5', () => {
+    client.sendMessage('5513991543069@c.us', 'Bom dia mãe, te amo').then(() => {
+      console.log('Mensagem para mãe enviada com sucesso!');
+    });
+
+    client.sendMessage('5513981219910@c.us', 'Bom dia pai, te amo').then(() => {
+      console.log('Mensagem para pai enviada com sucesso!');
+    });
+
+    client.sendMessage('5513981529487@c.us', 'Boom dia nenem').then(() => {
+      console.log('Mensagem para Amor enviada!');
+    });
+  });
+  console.log('WhatsApp authenticated.')
+});
 client.on('auth_failure', () => console.log('WhatsApp authentication failed.'));
 client.on('disconnected', () => console.log('WhatsApp lost connection.'));
+
 
 client.initialize();
 
